d7d7dba8958765eb6d473a12f98a634c12cc3e68
AmancioRon <160134032+AmancioRon@users.noreply.github.com>
2024-03-12 15:25:08 +0800

    LIVE ACITIVITY

---
+++ ALDEVEVERA/src/aldevevera/Swordsman.java
@@ -0,0 +1,10 @@
+
+package aldevevera;
+
+
+public class Swordsman extends Character {
+    
+    public Swordsman(int health, int damage, int shield) {
+        super (health, damage, shield);
+    }
+}
